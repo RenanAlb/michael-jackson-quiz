d62832ef8854c169dbc46e6b1b7e6dba17898cc7
Renan Albuquerque <renan.albuquerque2501@gmail.com>
2024-10-04 15:21:32 -0300

    Alterações e melhorias feitas

--- ball.js
+++ ball.js
@@ -1,5 +1,6 @@
 const mouse = document.createElement('div');
 mouse.classList.add('mouse');
+const byElement = document.querySelector('.by');
 
 let mouseX = 0;
 let mouseY = 0;
@@ -26,6 +27,33 @@ function hoverElements() {
   });
   button.addEventListener('mouseleave', () => {
     button.innerText = 'Start quiz';
+    mouse.style.width = '160px';
+    mouse.style.height = '160px';
+  });
+
+  byElement.addEventListener('mouseenter', () => {
+    byElement.innerHTML = `
+      <p class="school">Escola - ETEC Eudécio Luiz Vicente - Adamantina</p>
+      <p class="teacher">Aline Borro - Professora de Inglês</p>
+      <p class="students">
+        Estudantes - Renan Albuquerque, Otávio Fernanado, Henrique Cardoso dos
+        Santos, João Pedro Passarini e João Vitor Gavazzi Veiga Amaro
+      </p>
+      <p class="code">Código de Renan Albuquerque</p>
+    `;
+    mouse.style.width = '600px';
+    mouse.style.height = '600px';
+  });
+  byElement.addEventListener('mouseleave', () => {
+    byElement.innerHTML = `
+      <p class="school">School - ETEC Eudécio Luiz Vicente - Adamantina</p>
+      <p class="teacher">Aline Borro - English teacher</p>
+      <p class="students">
+        Students - Renan Albuquerque, Otávio Fernanado, Henrique Cardoso dos
+        Santos, João Pedro Passarini and João Vitor Gavazzi Veiga Amaro
+      </p>
+      <p class="code">Code by Renan Albuquerque</p>
+    `;
     mouse.style.width = '160px';
     mouse.style.height = '160px';
   });
